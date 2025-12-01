f32614475591a7d6d33a0b14af3e9c39039da783
Guilherme Storch de Oliveira <gsoliveira@brasal.com.br>
2025-12-01 10:44:38 -0300

    [879] estrutura inicial do problema

---
+++ 879.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+int profitableSchemes(int n, int minProfit, int* group, int groupSize, int* profit, int profitSize){
+    (void)n;
+    (void)minProfit;
+    (void)group;
+    (void)groupSize;
+    (void)profit;
+    (void)profitSize;
+
+    // TODO:
+    // DP 2D: dp[people][p] = nº de esquemas usando 'people' pessoas e lucro p (capado em minProfit).
+
+    return 0;
+}
