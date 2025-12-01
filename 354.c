99aa23dc16d436692d68fd4c917dcb24ad4d0d36
Guilherme Storch de Oliveira <gsoliveira@brasal.com.br>
2025-12-01 08:20:39 -0300

    [304] Stub com assinatura

---
+++ 354.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int maxEnvelopes(int** envelopes, int envelopesSize, int* envelopesColSize){
+    (void)envelopes;
+    (void)envelopesSize;
+    (void)envelopesColSize;
+
+    // TO-DO:
+    // 1. Ordenar envelopes.
+    // 2. Aplicar LIS nas alturas.
+
+    return 0;
+}
