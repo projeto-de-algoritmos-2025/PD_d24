2d32f14e8df906d7ecbd78c4a3d8a73098e02d9d
Guilherme Storch de Oliveira <gsoliveira@brasal.com.br>
2025-12-01 11:33:21 -0300

    Inicialização da DP

--- 879.c
+++ 879.c
@@ -1,15 +1,18 @@
 #include <string.h>
 
 int profitableSchemes(int n, int minProfit, int* group, int groupSize, int* profit, int profitSize){
-    (void)n;
-    (void)minProfit;
     (void)group;
     (void)groupSize;
     (void)profit;
     (void)profitSize;
 
-    // TODO:
-    // DP 2D: dp[people][p] = nº de esquemas usando 'people' pessoas e lucro p (capado em minProfit).
+    static int dp[101][101]; // placeholder pelos limites, ainda sem usar crimes
+    memset(dp, 0, sizeof(dp));
 
+    dp[0][0] = 1; // um jeito de ter lucro 0 com 0 pessoas (não fazer nada)
+
+    // Ainda não iteramos sobre os crimes
+    // Como teste, retornamos dp[0][0] se minProfit == 0
+    if (minProfit == 0) return 1;
     return 0;
 }
